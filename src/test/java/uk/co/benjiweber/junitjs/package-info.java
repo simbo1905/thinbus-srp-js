0ba681772abba06577ea84ee44cde0d35e828a45
Simon Massey <simbo1905@60hertz.com>
2014-06-03 23:40:02 +0100

    package-info.java for junit-js

---
+++ src/test/java/uk/co/benjiweber/junitjs/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * junit-js add a test runner which can run js test files on jdk1.7 Rhino or jdk1.8 Nashorn
+ * See http://benjiweber.co.uk/blog/2013/01/27/javascript-tests-with-junit/
+ */
+package uk.co.benjiweber.junitjs;
