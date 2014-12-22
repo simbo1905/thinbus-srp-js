bee16ff2fcffc67673297d2d315f1f438a0c0068
bernard.wittwer <bernard.wittwer@leshop.ch>
2014-12-22 16:39:17 +0100

    added access to userId in the underlying nimbus SRP6Session

--- src/main/java/com/bitbucket/thinbus/srp6/js/SRP6JavascriptServerSession.java
+++ src/main/java/com/bitbucket/thinbus/srp6/js/SRP6JavascriptServerSession.java
@@ -1,7 +1,6 @@
 package com.bitbucket.thinbus.srp6.js;
 
-import static com.nimbusds.srp6.BigIntegerUtils.fromHex;
-import static com.nimbusds.srp6.BigIntegerUtils.toHex;
+import static com.nimbusds.srp6.BigIntegerUtils.*;
 
 import java.math.BigInteger;
 
@@ -15,20 +14,16 @@ abstract public class SRP6JavascriptServerSession {
 
 	/**
 	 * Increments this SRP-6a authentication session to {@link State#STEP_1}.
-	 * 
+	 *
 	 * @param username
-	 *            The identity 'I' of the authenticating user. Must not be
-	 *            {@code null} or empty.
+	 *            The identity 'I' of the authenticating user. Must not be {@code null} or empty.
 	 * @param salt
 	 *            The password salt 's'. Must not be {@code null}.
 	 * @param v
 	 *            The password verifier 'v'. Must not be {@code null}.
-	 * 
 	 * @return The server public value 'B' as hex encoded number.
-	 * 
 	 * @throws IllegalStateException
-	 *             If the mehod is invoked in a state other than
-	 *             {@link State#INIT}.
+	 *             If the mehod is invoked in a state other than {@link State#INIT}.
 	 */
 	public String step1(final String username, final String salt, final String v) {
 		BigInteger B = session.step1(username, fromHex(salt), fromHex(v));
@@ -37,22 +32,17 @@ abstract public class SRP6JavascriptServerSession {
 
 	/**
 	 * Increments this SRP-6a authentication session to {@link State#STEP_2}.
-	 * 
+	 *
 	 * @param A
 	 *            The client public value. Must not be {@code null}.
 	 * @param M1
 	 *            The client evidence message. Must not be {@code null}.
-	 * 
-	 * @return The server evidence message 'M2' has hex encoded number with
-	 *         leading zero padding to match the 256bit hash length.
-	 * 
+	 * @return The server evidence message 'M2' has hex encoded number with leading zero padding to match the 256bit hash
+	 *         length.
 	 * @throws SRP6Exception
-	 *             If the client public value 'A' is invalid or the user
-	 *             credentials are invalid.
-	 * 
+	 *             If the client public value 'A' is invalid or the user credentials are invalid.
 	 * @throws IllegalStateException
-	 *             If the mehod is invoked in a state other than
-	 *             {@link State#STEP_1}.
+	 *             If the mehod is invoked in a state other than {@link State#STEP_1}.
 	 */
 	public String step2(final String A, final String M1) throws Exception {
 		BigInteger M2 = session.step2(fromHex(A), fromHex(M1));
@@ -63,7 +53,7 @@ abstract public class SRP6JavascriptServerSession {
 
 	/**
 	 * Returns the underlying session state as a String for JavaScript testing.
-	 * 
+	 *
 	 * @return The current state.
 	 */
 	public String getState() {
@@ -71,28 +61,31 @@ abstract public class SRP6JavascriptServerSession {
 	}
 
 	/**
-	 * The crypto parameters for the SRP-6a protocol. These must be agreed
-	 * between client and server before authentication and consist of a large
-	 * safe prime 'N', a corresponding generator 'g' and a hash function
-	 * algorithm 'H'. You can generate your own with openssl using
-	 * {@link OpenSSLCryptoConfig}
-	 * 
+	 * Gets the identity 'I' of the authenticating user.
+	 *
+	 * @return The user identity 'I', null if undefined.
+	 */
+	public String getUserID() {
+		return session.getUserID();
+	}
+
+	/**
+	 * The crypto parameters for the SRP-6a protocol. These must be agreed between client and server before
+	 * authentication and consist of a large safe prime 'N', a corresponding generator 'g' and a hash function algorithm
+	 * 'H'. You can generate your own with openssl using {@link OpenSSLCryptoConfig}
 	 */
 	protected final SRP6CryptoParams config;
 
 	/**
-	 * The underlying Nimbus session which will be configure for JavaScript
-	 * interactions
+	 * The underlying Nimbus session which will be configure for JavaScript interactions
 	 */
 	protected final SRP6ServerSession session;
-	
+
 	/**
-	 * Constructs a JavaScript compatible server session which configures an
-	 * underlying Nimbus SRP6ServerSession.
-	 * 
+	 * Constructs a JavaScript compatible server session which configures an underlying Nimbus SRP6ServerSession.
+	 *
 	 * @param srp6CryptoParams
-	 *            cryptographic constants which must match those being used by
-	 *            the client.
+	 *            cryptographic constants which must match those being used by the client.
 	 */
 	public SRP6JavascriptServerSession(SRP6CryptoParams srp6CryptoParams) {
 		this.config = srp6CryptoParams;
@@ -103,11 +96,9 @@ abstract public class SRP6JavascriptServerSession {
 	}
 
 	/**
-	 * k is actually fixed and done with hash padding routine which uses
-	 * java.net.BigInteger byte array constructor so this is a convenience
-	 * method to get at the Java generated value to use in the configuration of
-	 * the Javascript
-	 * 
+	 * k is actually fixed and done with hash padding routine which uses java.net.BigInteger byte array constructor so
+	 * this is a convenience method to get at the Java generated value to use in the configuration of the Javascript
+	 *
 	 * @return 'k' calculated as H( N, g )
 	 */
 	public String k() {
@@ -116,7 +107,7 @@ abstract public class SRP6JavascriptServerSession {
 
 	/**
 	 * Turn a radix10 string into a java.net.BigInteger
-	 * 
+	 *
 	 * @param base10
 	 *            the radix10 string
 	 * @return the BigInteger representation of the number
@@ -131,12 +122,10 @@ abstract public class SRP6JavascriptServerSession {
 	public static int HASH_HEX_LENGTH;
 
 	/**
-	 * Outputs the configuration in the way which can be used to configure
-	 * JavaScript.
-	 * 
-	 * Note that 'k' is fixed but uses the byte array constructor of BigInteger
-	 * which is not available in JavaScript to you must set it as configuration.
-	 * 
+	 * Outputs the configuration in the way which can be used to configure JavaScript. Note that 'k' is fixed but uses
+	 * the byte array constructor of BigInteger which is not available in JavaScript to you must set it as
+	 * configuration.
+	 *
 	 * @return Parameters required by JavaScript client.
 	 */
 	@Override
