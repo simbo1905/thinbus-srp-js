d11a3b4265a6526131929e4be077f2e9750adba0
bernard.wittwer <bernard.wittwer@leshop.ch>
2015-06-08 09:23:42 +0200

    Added unit test for ServerSession serialization

--- src/test/java/com/bitbucket/thinbus/srp6/js/JavaClientTests.java
+++ src/test/java/com/bitbucket/thinbus/srp6/js/JavaClientTests.java
@@ -4,6 +4,11 @@ import static com.nimbusds.srp6.BigIntegerUtils.fromHex;
 import static com.nimbusds.srp6.BigIntegerUtils.toHex;
 import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 
@@ -11,6 +16,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import com.nimbusds.srp6.SRP6ClientCredentials;
+import com.nimbusds.srp6.SRP6ServerSession;
 
 public class JavaClientTests {
 
@@ -112,6 +118,112 @@ public class JavaClientTests {
 		Assert.assertEquals(cK, sK);
 	}
 
+	@Test
+	public void testServerSessionSerialization() throws Exception {
+		SRP6JavaClientSessionSHA256 client = new SRP6JavaClientSessionSHA256(
+				N_base10, g_base10);
+
+		String salt = client
+				.generateRandomSalt(SRP6JavascriptServerSessionSHA256.HASH_HEX_LENGTH);
+
+		HexHashedVerifierGenerator generator = new HexHashedVerifierGenerator(
+				N_base10, g_base10, SRP6JavascriptServerSessionSHA256.SHA_256);
+
+		String v = generator.generateVerifier(salt, username, password);
+
+		client.step1(username, password);
+
+		SRP6JavascriptServerSession server = new SRP6JavascriptServerSessionSHA256(
+				N_base10, g_base10);
+
+		String B = server.step1(username, salt, v);
+
+		// Simulate session serialization
+		byte[] serializedSession = toByteArray(server);
+		// Creates a copy of the session from a serialized byte array
+		SRP6JavascriptServerSession serverCopy = fromByteArray(serializedSession);
+		
+		SRP6ClientCredentials credentials = client.step2(salt, B);
+
+		String M2 = serverCopy.step2(toHex(credentials.A), toHex(credentials.M1));
+
+		client.step3(M2);
+		
+		// both share a strong session key.
+		String cS = client.getSessionKey(false);
+		String sS = serverCopy.getSessionKey(false);
+		Assert.assertEquals(cS, sS);
+		
+		// the hash value may be more useful as a secret key. 
+		String cK = client.getSessionKey(true);
+		String sK = serverCopy.getSessionKey(true);
+		Assert.assertEquals(cK, sK);
+	}
+	
+	//	
+	//	 Use a ObjectOutputStream to perform serialization
+	//	 
+	protected byte[] toByteArray(SRP6JavascriptServerSession session) throws IOException {
+		ObjectOutputStream oos = null;
+		ByteArrayOutputStream byteStream = null;
+		try {
+			byteStream = new ByteArrayOutputStream();
+			oos = new ObjectOutputStream(byteStream);
+			oos.writeObject(session);
+			oos.flush();
+			return byteStream.toByteArray();
+		} catch (IOException e) {
+			throw e;
+		} finally {
+			try {
+				if (oos != null) {
+					oos.flush();
+					oos.close();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+			try {
+				if (byteStream != null) {
+					byteStream.close();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+	
+	//
+	//	  Use a ObjectInputStream to perform deserialization
+	//	 
+	protected SRP6JavascriptServerSession fromByteArray(byte[] bytes) throws Exception  {
+		ObjectInputStream ois = null;
+		ByteArrayInputStream byteStream = null;
+		try {
+			byteStream = new ByteArrayInputStream(bytes);
+			ois = new ObjectInputStream(byteStream);
+			SRP6JavascriptServerSession result = (SRP6JavascriptServerSession) ois.readObject();
+			return result;
+		} catch (IOException | ClassNotFoundException e) {
+			throw e;
+		} finally {
+			try {
+				if (ois != null) {
+					ois.close();
+				}
+			} catch (final IOException ex) {
+				ex.printStackTrace();
+			}
+			try {
+				if (byteStream != null) {
+					byteStream.close();
+				}
+			} catch (final IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+	
 	// this is a brute force comparison that javascript and Java can login
 	// against each other
 	public static void main(String[] args) throws Exception {
