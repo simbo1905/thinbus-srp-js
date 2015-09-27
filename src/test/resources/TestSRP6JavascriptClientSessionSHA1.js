2d1e15ca503e1d0658a6d39f33f887c4d89cb1ac
simbo1905 <simbo1905@60hertz.com>
2015-09-27 08:44:30 +0100

    beefed up the number of mutual verifications in the tests

--- src/test/resources/TestSRP6JavascriptClientSessionSHA1.js
+++ src/test/resources/TestSRP6JavascriptClientSessionSHA1.js
@@ -13,9 +13,10 @@ load("src/main/resources/js/thinbus-srp6client.js");
 
 // ** you must define crypo params before importing the particular configuration thinbus-srp6a-config*.js and they must match the java server config **
 var SRP6CryptoParams= {
-	N_base10: "19502997308733555461855666625958719160994364695757801883048536560804281608617712589335141535572898798222757219122180598766018632900275026915053180353164617230434226106273953899391119864257302295174320915476500215995601482640160424279800690785793808960633891416021244925484141974964367107",
+	// WARNING this is a tiny 256 bit prime DO NOT COPY THIS it is recommended to use 2048 bit safe primes
+	N_base10: "4952044212259374630169507547594526520630503506396470834812363719692719",
 	g_base10: "2", 
-	k_base16: "8d7c38a15a345fc1285b7b5a9e704e0587329ed8"
+	k_base16: "a2ebd09734ae9220587a89c7eb230dec95169bce"
 }
 
 // import config for test
@@ -38,35 +39,56 @@ tests({
 	See the comments in the SHA256 version of this class for a fuller description.  
 	*/
 	testMutualAuthentiation: function() {
+		
+//		println(username);
+//		println(password);
+//		println(SRP6CryptoParams.g_base10);
+//		println(SRP6CryptoParams.N_base10);
+//		println(SRP6CryptoParams.k_base16);
 	
-		var client = new SRP6JavascriptClientSessionSHA1();
-		
-		var salt = client.generateRandomSalt(); // consider passing server secure random to this method
-		
-		var v = client.generateVerifier(salt, username, password);
-		client.step1(username,password);
-		
-		var server = new javaServerSession(SRP6CryptoParams.N_base10, SRP6CryptoParams.g_base10);
-		var B = server.step1(username, salt, v);
-		
-		var credentials = client.step2(salt, B);
-		
-		var M2 = server.step2(credentials.A, credentials.M1);
-		
-		client.step3(M2);
-		
-		var cS = client.getSessionKey(false);
-		
-		var sS = server.getSessionKey(false);
-
-		assert.assertTrue(cS == sS); 
-		
-		var cK = client.getSessionKey();
-		
-		var sK = server.getSessionKey(true);
-		
-		assert.assertTrue(cK == sK); 
+		// run this 100 times to catch any problems with dropping leading zeros in BigDecimal conversions to and from hex
+		for( var i = 0; i < 100; i++) {
+			var client = new SRP6JavascriptClientSessionSHA1();
+			
+			var salt = client.generateRandomSalt(); // consider passing server secure random to this method
+			
+			//println(""+salt);
+			
+			var v = client.generateVerifier(salt, username, password);
+			
+			//println(v);
+			
+			client.step1(username,password);
+			
+			var server = new javaServerSession(SRP6CryptoParams.N_base10, SRP6CryptoParams.g_base10);
+			var B = server.step1(username, salt, v);
+			
+//			println(server.b());
+//			println(B);
+			
+			var credentials = client.step2(salt, B);
+			
+//			println(credentials.A);
+//			println(credentials.M1);
+			
+			var M2 = server.step2(credentials.A, credentials.M1);
+			
+//			println(M2);
+			
+			client.step3(M2);
+			
+			var cS = client.getSessionKey(false);
+			
+			var sS = server.getSessionKey(false);
+			
+			assert.assertTrue(cS == sS); 
+			
+			var cK = client.getSessionKey();
+			
+			var sK = server.getSessionKey(true);
+			
+			assert.assertTrue(cK == sK); 
+		}
 	}
-	
 });
 
