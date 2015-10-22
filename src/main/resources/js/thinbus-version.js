91fc914613061e936709e60545a73f1f6977ebbb
simbo1905 <simbo1905@60hertz.com>
2015-10-22 21:51:45 +0100

    adding project version into the main js file

---
+++ src/main/resources/js/thinbus-version.js
@@ -0,0 +1,6 @@
+/*
+ * Thinbus Javascript Secure Remote Password (SRP)
+ * https://bitbucket.org/simon_massey/thinbus-srp-js
+ * Version  ${project.version}
+ * Copyright 2014-2015 Simon Massey http://www.apache.org/licenses/LICENSE-2.0
+ */
