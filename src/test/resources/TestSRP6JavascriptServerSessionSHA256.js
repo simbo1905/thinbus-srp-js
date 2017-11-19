00676bb3f3b752c20794ea6a8b1611b0f1b7d2e9
simbo1905 <simbo1905@60hertz.com>
2017-11-19 21:59:08 +0000

    reduced iterations on js roundtrip as takes to long to run on codeship.com CI. bumpted pom version to 1.5.2-SNAPSHOT

--- src/test/resources/TestSRP6JavascriptServerSessionSHA256.js
+++ src/test/resources/TestSRP6JavascriptServerSessionSHA256.js
@@ -29,39 +29,38 @@ tests({
 	*/
 	testMutualAuthentication: function() {
 
-	    for( var i = 0; i < 4; i++) {
-	
-            // client constructor requires a variable SRP6CryptoParams is defined which sets N, g, k
-            var client = new SRP6JavascriptClientSessionSHA256();
 
-            // server constructor requires a variable SRP6CryptoParams is defined which sets N, g, k
-            var server = new SRP6JavascriptServerSessionSHA256();
+        // client constructor requires a variable SRP6CryptoParams is defined which sets N, g, k
+        var client = new SRP6JavascriptClientSessionSHA256();
 
-            // random salt is created at user first registration.
-            var salt = client.generateRandomSalt(); // consider passing server secure random to this method
+        // server constructor requires a variable SRP6CryptoParams is defined which sets N, g, k
+        var server = new SRP6JavascriptServerSessionSHA256();
 
-            // verifier to be generated at the browser during user registration and password (or email address) reset only
-            var v = client.generateVerifier(salt, username, password);
+        // random salt is created at user first registration.
+        var salt = client.generateRandomSalt(); // consider passing server secure random to this method
 
-            // normal login flow step1a client: browser starts with username and password given by user at the browser
-            client.step1(username,password);
+        // verifier to be generated at the browser during user registration and password (or email address) reset only
+        var v = client.generateVerifier(salt, username, password);
 
-            // normal login flow step1b server: server starts with username from browser plus salt and verifier saved to database on user registration.
-            var B = server.step1(username, salt, v);
+        // normal login flow step1a client: browser starts with username and password given by user at the browser
+        client.step1(username,password);
 
-            // normal login flow step2a client: server sends users salt from user registration and the server ephemeral number
-            var credentials = client.step2(salt, B);
+        // normal login flow step1b server: server starts with username from browser plus salt and verifier saved to database on user registration.
+        var B = server.step1(username, salt, v);
 
-            // normal login flow step2b server: client sends its client ephemeral number and proof of a shared session key derived from both ephermal numbers and the password
-            var M2 = server.step2(credentials.A, credentials.M1);
+        // normal login flow step2a client: server sends users salt from user registration and the server ephemeral number
+        var credentials = client.step2(salt, B);
 
-            // normal login flow step3 client: client verifies that the server shows proof of the shared session key which demonstrates that it knows actual verifier
-            client.step3(M2);
+        // normal login flow step2b server: client sends its client ephemeral number and proof of a shared session key derived from both ephermal numbers and the password
+        var M2 = server.step2(credentials.A, credentials.M1);
 
-            // the javascript client defaults to hashing the session key as that is additional protection of the password in case the key is accidentally exposed to an attacker.
-            assert.assertTrue(client.getSessionKey() == client.getSessionKey(true));
+        // normal login flow step3 client: client verifies that the server shows proof of the shared session key which demonstrates that it knows actual verifier
+        client.step3(M2);
 
-		}
+        // the javascript client defaults to hashing the session key as that is additional protection of the password in case the key is accidentally exposed to an attacker.
+        assert.assertTrue(client.getSessionKey() == client.getSessionKey(true));
+
+
 	}
 	
 });
